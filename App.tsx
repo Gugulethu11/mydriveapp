ec7ed250ecd230e4e6460e9451054632e937251a
Gugulethu11 <ST10467398@vcconnect.edu.za>
2025-09-03 14:54:11 +0200

    we have added license years

--- App.tsx
+++ App.tsx
@@ -1,11 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const age = 24;
+  const licenseyears = 1;
+  let feedback = '';
+
+  if (age >= 18) 
+    if (licenseyears >= 3) 
+      {
+        feedback = 'You are a qualified, safe driver.';
+      }
+        else
+      {
+          feedback = 'You are allowed to drive. YAY!'
+      } 
+        else 
+      {
+          feedback = 'You are not yet eligible to drive.'
+      }   
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <Text style={styles.response}> {feedback} {/* This is my message to be displayed to the user*/} </Text> 
+     
     </View>
   );
 }
@@ -13,8 +33,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'blue',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  response: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: 'white',
+  }
 });
